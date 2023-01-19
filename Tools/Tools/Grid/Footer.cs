6877a5b01f70477f7b59f5fd7b77d103ccb2a0f7
Mohamed-Abuelatta <abuelatta.shesht@gmail.com>
2023-01-19 11:23:04 +0300

    .

--- Tools/Tools/Grid/Footer.cs
+++ Tools/Tools/Grid/Footer.cs
@@ -8,6 +8,6 @@
         public List<int> fRange { get; set; }
 
         public int lastPagerBtn { get; set; }
-        public int pageRowsCount { get; set; } 
+        public bool isFulllastPage { get; set; } 
     }
 }
