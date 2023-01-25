e9216751d772f43bf93ad2e38ae37b4814256ba0
Mohamed Abuelatta <mr.mohamed.abuelatta@outlook.com>
2023-01-26 00:04:39 +0300

    024

--- Tools/Tools/Grid/Footer.cs
+++ Tools/Tools/Grid/Footer.cs
@@ -3,8 +3,8 @@
     public class Footer
     {
         public string isPrevDisabled { get; set; }
-        public string isNextDisabled { get; set; } 
-        public int activeBtn { get; set; } 
+        public string isNextDisabled { get; set; }
+        public int activeBtn { get; set; } = 1; 
         public List<int> fRange { get; set; }
     }
 }
